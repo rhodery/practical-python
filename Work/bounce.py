8c9d0d8f678079a43a08467e12fa48f857611b10
Nick Hershberger <hershfamjunk@gmail.com>
2024-01-02 23:51:06 +0000

    Day 1 work

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce = .6
+
+for i in range(10):
+    height = height * bounce
+    print(i+1, round(height, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,30 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = float(input('Interest rate: '))
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment = float(input('Extra payment: '))
+extra_payment_start_month = float(input('Extra payment start month: '))
+extra_payment_end_month = float(input('Extra payment end month: '))
+
+while principal > 0:
+    month += 1
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
+        new_payment = payment + extra_payment
+    else:
+        new_payment = payment
+
+    if principal < new_payment:
+        new_payment = principal
+        principal = 0
+    else:
+        principal = principal * (1+rate/12) - new_payment
+    
+    total_paid = total_paid + new_payment
+
+    print(month, total_paid, principal)
+
+print('Total paid:', round(total_paid, 2), 'Months:', month)
